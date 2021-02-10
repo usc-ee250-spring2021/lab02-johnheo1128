8825246f4d082e0761d664c87fbae624104dbdbb
johnheo1128 <johnheo@usc.edu>
2021-02-09 20:55:52 -0800

    updates

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -4,7 +4,7 @@ List team members here.
 
 Insert Github repository link here.
 """
-https://github.com/usc-ee250-spring2021/lab02-johnheo1128 
+#https://github.com/usc-ee250-spring2021/lab02-johnheo1128 
 """python3 interpreters in Ubuntu (and other linux distros) will look in a 
 default set of directories for modules when a program tries to `import` one. 
 Examples of some default directories are (but not limited to):
@@ -27,24 +27,27 @@ from grove_rgb_lcd import *
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
-set text("Hello world\nLCD test")
-setRGB(0,128,64)
-setRGB(0,255,0)
+
 if __name__ == '__main__':
 
-  #Connect ultrasounic sensor to D4
-  ultrasonic = 4
-  #Connect rotary angle sensor to A2
-  potentiometer = 2 
+#Connect ultrasounic sensor to D4
+ultrasonic = 4
+#Connect rotary angle sensor to A2
+potentiometer = 2 
 
-  while True:
-    #So we do not poll the sensors too quickly which may introduce noise,
-    #sleep for a reasonable time of 200ms between each iteration.
-    time.sleep(0.2)
-    # Read angle from potentiometer
-    i = grovepi.analogRead(potentiometer)
-    # Read distance value from Ultrasonic
-    j = grovepi.ultrasonicRead(ultrasonic)
-    setText_norefresh("pot:" + i + "         " + "dist:" + j "        ")
+oled_init()
+oled_clearDisplay()
+oled_setNormalDisplay()
+oled_setVerticalMode()
+time.sleep(.1)
+while True:
+  #So we do not poll the sensors too quickly which may introduce noise,
+  #sleep for a reasonable time of 200ms between each iteration.
+  time.sleep(0.5)
+  # Read angle from potentiometer
+  print(grovepi.analogRead(potentiometer))
+  # Read distance value from Ultrasonic
+  print(grovepi.ultrasonicRead(ultrasonic) + "\n")
+
 
 
